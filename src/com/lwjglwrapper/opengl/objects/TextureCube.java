ec050677bd27d67acbb7c669f0d90051d2570660
ngoduyanh <ngoduyanh.chip@gmail.com>
2019-09-08 19:40:31 +0700

    Added resource loading for cube texture

--- src/com/lwjglwrapper/opengl/objects/TextureCube.java
+++ src/com/lwjglwrapper/opengl/objects/TextureCube.java
@@ -15,21 +15,29 @@ import org.lwjgl.opengl.GL13;
  */
 public class TextureCube extends Texture{
     
-    public TextureCube(String f0, String f1, String f2, String f3, String f4, String f5) {
+    public static final int FILE_MODE = 0, RESOURCE_MODE = 1;
+    
+    public TextureCube(int pathMode, String f0, String f1, String f2, String f3, String f4, String f5) {
         super(GL13.GL_TEXTURE_CUBE_MAP);
         bind(0);
         String[] paths = {f0, f1, f2, f3, f4, f5};
         for (int i = 0; i < paths.length; i++) {
             String path = paths[i];
-            TextureData data = TextureData.fromPath(path);
+            TextureData data;
+            if(pathMode == FILE_MODE) {
+                data = TextureData.fromPath(path);
+            } else {
+                data = TextureData.fromResource(TextureCube.class, path);
+            }
             GL11.glTexImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL11.GL_RGBA,
                     data.width, data.height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data.pixels);
         }
         configTexture(gl_id);
     }
     
-    public TextureCube(String dir, String ext, String f0, String f1, String f2, String f3, String f4, String f5) {
-        this(dir + "/" + f0 + "." + ext,
+    public TextureCube(int pathMode, String dir, String ext, String f0, String f1, String f2, String f3, String f4, String f5) {
+        this(pathMode,
+             dir + "/" + f0 + "." + ext,
              dir + "/" + f1 + "." + ext,
              dir + "/" + f2 + "." + ext,
              dir + "/" + f3 + "." + ext,
@@ -37,8 +45,8 @@ public class TextureCube extends Texture{
              dir + "/" + f5 + "." + ext);
     }
     
-    public TextureCube(String dir, String ext) {
-        this(dir, ext, "right", "left", "top", "bottom", "back", "front");
+    public TextureCube(int pathMode, String dir, String ext) {
+        this(pathMode, dir, ext, "right", "left", "top", "bottom", "back", "front");
     }
 
     @Override
